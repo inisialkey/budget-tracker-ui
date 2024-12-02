02e4d3d0acb6505bc99bd4e57c5101912f6e8250
inisialkey <okidorvel404@gmail.com>
2024-12-02 15:21:04 +0700

    add bottomnav, daily page

---
+++ lib/json/daily_json.dart
@@ -0,0 +1,32 @@
+const List daily = [
+  {
+    "icon": "assets/images/img_bank.png",
+    "name": "Bank",
+    "date": "Fri 10AM",
+    "price": "\$340.00"
+  },
+  {
+    "icon": "assets/images/img_auto.png",
+    "name": "Automobile",
+    "date": "Mon 8AM",
+    "price": "\$70.00"
+  },
+  {
+    "icon": "assets/images/img_gift.png",
+    "name": "Gift",
+    "date": "Sat 6PM",
+    "price": "\$110.00"
+  },
+  {
+    "icon": "assets/images/img_eating.png",
+    "name": "Eating",
+    "date": "Sun 5PM",
+    "price": "\$60.00"
+  },
+  {
+    "icon": "assets/images/img_charity.png",
+    "name": "Charity",
+    "date": "Wed 12PM",
+    "price": "\$1200.00"
+  }
+];
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,78 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:budget_tracker_ui/pages/daily_page.dart';
+import 'package:budget_tracker_ui/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  List<Widget> pages = [
+    const DailyPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+      bottomNavigationBar: getFooter(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          selectedTab(4);
+        },
+        backgroundColor: Colors.pink,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.add,
+          size: 25,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: pages,
+    );
+  }
+
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      Ionicons.md_calendar,
+      Ionicons.md_stats_chart,
+      Ionicons.md_wallet,
+      Ionicons.ios_person,
+    ];
+
+    return AnimatedBottomNavigationBar(
+      activeColor: primary,
+      splashColor: secondary,
+      inactiveColor: Colors.black.withOpacity(0.5),
+      icons: iconItems,
+      activeIndex: pageIndex,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.softEdge,
+      leftCornerRadius: 10,
+      iconSize: 25,
+      rightCornerRadius: 10,
+      onTap: (index) {
+        selectedTab(index);
+      },
+      //other params
+    );
+  }
+
+  selectedTab(index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+}
